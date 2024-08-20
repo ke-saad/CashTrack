8f5588b938f5299a6c3b367c8ffe3d45cc01cb85
ke-saad <kellalisaad@gmail.com>
2024-08-20 01:49:49 +0100

    new file:   app/(dashboard)/layout.tsx 	modified:   app/(dashboard)/page.tsx 	new file:   app/api/[[...route]]/authors.ts 	new file:   app/api/[[...route]]/books.ts 	modified:   app/api/[[...route]]/route.ts 	new file:   components/header-logo.tsx 	new file:   components/header.tsx 	new file:   components/nav-button.tsx 	new file:   components/navigation.tsx 	new file:   components/ui/sheet.tsx 	modified:   middleware.ts 	modified:   package-lock.json 	modified:   package.json

--- app/api/[[...route]]/route.ts
+++ app/api/[[...route]]/route.ts
@@ -1,38 +1,15 @@
-import { z } from 'zod'
-import { zValidator } from '@hono/zod-validator'
 import { Hono } from 'hono'
 import { handle } from 'hono/vercel'
+
+import authors from './authors'
+import books from './books'
 
 export const runtime = 'edge'
 
 const app = new Hono().basePath('/api')
 
-app
-  .get('/hello', (c) => {
-    return c.json({
-      message: 'Hello Next.js!',
-    });
-  })
-  .get(
-    '/hello/:test',
-    (c) => {
-      return c.json({
-        message: 'Hello World',
-      })
-    })
-  .post(
-    '/create/:postId',
-    zValidator('json', z.object({
-      name: z.string(),
-      userId: z.number()
-    })),
-    zValidator('param', z.object({
-      postId: z.number(),
-    })),
-    (c) => {
-      const { name, userId } = c.req.valid('json');
-      const { postId } = c.req.valid('param');
-      return c.json({});
-    })
+app.route('/authors', authors);
+app.route('/books', books);
+
 export const GET = handle(app)
 export const POST = handle(app)
